aecd49cd2c474f500693ed5f7edde0ce5befea80
bsvdoom <flowtex@gmail.com>
2023-10-08 20:18:11 +0200

    Update BleKeyboard.h hu

--- BleKeyboard.h
+++ BleKeyboard.h
@@ -119,6 +119,21 @@ const MediaKeyReport KEY_MEDIA_WWW_BACK = {0, 32};
 const MediaKeyReport KEY_MEDIA_CONSUMER_CONTROL_CONFIGURATION = {0, 64}; // Media Selection
 const MediaKeyReport KEY_MEDIA_EMAIL_READER = {0, 128};
 
+// hu_HU keys
+#define KEY_O_ACUTE             (136+0x2e)
+#define KEY_O_UMLAUT            (136+0x27)
+#define KEY_O_DOUBLE_ACUTE      (136+0x2f)
+
+#define KEY_U_ACUTE             (136+0x30)
+#define KEY_U_UMLAUT            (136+0x2d)
+#define KEY_U_DOUBLE_ACUTE      (136+0x31)
+
+#define KEY_A_ACUTE             (136+0x34)
+
+#define KEY_E_ACUTE             (136+0x33)
+
+#define KEY_I_ACUTE             (136+0x32)
+
 
 //  Low level key report: up to 6 keys and shift, ctrl etc at once
 typedef struct
